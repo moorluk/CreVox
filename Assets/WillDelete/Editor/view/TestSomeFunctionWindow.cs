45cc39d2bcb71e87b0c812192449ebc0e6ef69bf
qwe321qwe321qwe321 <aegis321123@gmail.com>
2017-04-18 13:01:16 +0800

    update

--- Assets/WillDelete/Editor/view/TestSomeFunctionWindow.cs
+++ Assets/WillDelete/Editor/view/TestSomeFunctionWindow.cs
@@ -12,16 +12,36 @@ namespace Test {
 		private static List<GraphGrammarNode> alphabets = new List<GraphGrammarNode>();
 		private static List<VolumeData> vdatas = new List<VolumeData>();
 
-		void Awake() {
+		void Initial() {
 			alphabets.Clear();
 			vdatas.Clear();
 			foreach (var node in Alphabet.Nodes) {
-				if(node == Alphabet.AnyNode || node.Terminal == NodeTerminalType.NonTerminal) {
+				if (node == Alphabet.AnyNode || node.Terminal == NodeTerminalType.NonTerminal) {
 					continue;
 				}
 				alphabets.Add(node);
 				vdatas.Add(null);
 			}
+		}
+		void Awake() {
+			Initial();
+		}
+		void OnFocus() {
+			if (isChanged()) {
+				Initial();
+			}
+		}
+		bool isChanged() {
+			for (int i = 0, index = 0; i < Alphabet.Nodes.Count; i++) {
+				if (Alphabet.Nodes[i] == Alphabet.AnyNode || Alphabet.Nodes[i].Terminal == NodeTerminalType.NonTerminal) {
+					continue;
+				}
+				if (index >= alphabets.Count || Alphabet.Nodes[i].AlphabetID != alphabets[index].AlphabetID) {
+					return true;
+				}
+				index++;
+			}
+			return false;
 		}
 		void OnGUI() {
 			scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition, GUILayout.Width(Screen.width), GUILayout.Height(Screen.height * 0.75f));
