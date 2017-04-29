afdbeea474226b3cead16f5f5efd63af2c7bfa61
HarrisonYei <ydharrison0720@gmail.com>
2017-04-29 14:19:01 +0800

    Space replacement and alphabet window

---
+++ Assets/WillDelete/Editor/SpaceAlphabet.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using CreVox;
+using MissionGrammarSystem;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System;
+
+namespace CrevoxExtend {
+	public class SpaceAlphabet {
+		private static string _path = Environment.CurrentDirectory + @"\Assets\Resources\CreVox\VolumeArtPack\LevelPieces\4_Sign\ConnectionTypes/";
+		private static string prefabRegex = @".*[\\\/](\w+)_(\w+)\.prefab$";
+		private static List<string> alphabets = new List<string>() { "Connection" };
+		private static bool changed = true;
+
+		public static void Load() {
+			alphabets = new List<string>() { "Connection" };
+			string[] files = Directory.GetFiles(_path);
+			string matchFile;
+			for (int i = 0; i < files.Length; i++) {
+				if (Regex.IsMatch(files[i], prefabRegex)) {
+					matchFile = Regex.Match(files[i], prefabRegex).Groups[2].Value;
+					if (!alphabets.Exists(a => (a == matchFile))) {
+						alphabets.Add(matchFile);
+					}
+				}
+			}
+		}
+		public static void alphabetUpdate(List<string> newAlphabet) {
+			Load();
+			foreach (string s in newAlphabet) {
+				if (!alphabets.Exists(e => (e == s))) {
+					NewPrefab("Connection_" + s);
+				}
+			}
+
+			for(int i = alphabets.Count-1;i >= 0; i--) {
+				if (!newAlphabet.Exists(e => (e == alphabets[i]))) {
+					DeletePrefab("Connection_"+alphabets[i]);
+					alphabets.RemoveAt(i);
+				}
+			}
+			PaletteWindow window = EditorWindow.GetWindow<PaletteWindow>();
+			window.InitialPaletteWindow();
+		}
+		// File IO
+		public static void NewPrefab(string fileName) {
+			if(!File.Exists(_path + fileName + ".prefab")) {
+				File.Copy(_path + "Connection.prefab", _path + fileName + ".prefab");
+				AssetDatabase.ImportAsset(@"Assets\Resources\CreVox\VolumeArtPack\LevelPieces\4_Sign\ConnectionTypes/" + fileName + ".prefab");
+				GetPrefab(fileName).GetComponent<PaletteItem>().itemName = fileName;
+			}
+		}
+		public static void DeletePrefab(string fileName) {
+			AssetDatabase.DeleteAsset(@"Assets\Resources\CreVox\VolumeArtPack\LevelPieces\4_Sign\ConnectionTypes/" + fileName + ".prefab");
+		}
+		public static List<string> Alphabets {
+			get { return alphabets; }
+		}
+		private static GameObject GetPrefab(string name) {
+			return (Resources.Load(@"CreVox/VolumeArtPack/LevelPieces/4_Sign/ConnectionTypes/"+name) as GameObject);
+		}
+		public static bool Changed {
+			get { return changed; }
+			set { changed = value; }
+		}
+	}
+}
