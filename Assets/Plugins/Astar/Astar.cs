7378ec7e9529e0e664c64839105d27f59d389a95
eurekaqq <npes89033@gmail.com>
2017-05-17 18:33:22 +0800

    finished first version A* and fixed some bug in GA.cs

---
+++ Assets/Plugins/Astar/Astar.cs
@@ -0,0 +1,182 @@
+﻿using GC = System.GC;
+using Math = System.Math;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace CrevoxExtend {
+	public class AStar {
+		//show node state.
+		public enum nodeType {
+			wall = 0,
+			none = 1,
+			open = 2,
+			start = 3,
+			end = 4
+		}
+
+		//using node to show position state for A*.
+		public class Node {
+			public nodeType type {
+				get;
+				set;
+			}
+
+			public Vector3 position3 {
+				get;
+				private set;
+			}
+
+			public int X {
+				get;
+				set;
+			}
+			public int Y {
+				get;
+				set;
+			}
+
+			public int Z {
+				get;
+				set;
+			}
+			public float fScore {
+				get { return hScore + gScore; }
+			}
+
+			public float hScore {
+				get;
+				private set;
+			}
+
+			public int gScore {
+				get;
+				private set;
+			}
+
+			//constructor.
+			public Node(int X, int Y, int Z, nodeType type) {
+				this.position3 = new Vector3(X * 3, Y * 2, Z * 3);
+				this.type = type;
+				this.hScore = float.MaxValue;
+				this.gScore = int.MaxValue;
+				this.X = X;
+				this.Y = Y;
+				this.Z = Z;
+			}
+
+			//update the gscore and hscore,if gscore + hscore < current fscore.
+			public void distanceToEnd(Node end, int time) {
+				//if thie node is end, and then make this node fscore to  zero, and select it.
+				if (this.type == nodeType.end) {
+					this.gScore = 0;
+					this.hScore = 0;
+				}
+
+				var temp = Math.Max(Math.Abs(this.X - end.X), Math.Max(Math.Abs(this.Y - end.Y), Math.Abs(this.Z - end.Z)));
+				if (temp + time < this.fScore) {
+					this.hScore = temp;
+					this.gScore = time;
+				}
+			}
+
+			public override string ToString() {
+				return (position3.ToString());
+			}
+		}
+
+		//use nodeMap to show map of volume state.
+		public Node[,,] nodeMap {
+			get;
+			private set;
+		}
+
+		public List<Node> theShortestPath {
+			get;
+			private set;
+		}
+
+		public int time {
+			get;
+			private set;
+		}
+
+		//constructor
+		//when new it, it will do A* algorithm, and then you can call this object.theShortestPath to get the
+		//shortest path.
+		public AStar(int[,,] map, Vector3 start, Vector3 end) {
+			this.nodeMap = mapToNodeMap(map);
+			this.nodeMap[(int)start.x / 3, (int)start.y / 2, (int)start.z / 3].type = nodeType.start;
+			this.nodeMap[(int)end.x / 3, (int)end.y / 2, (int)end.z / 3].type = nodeType.end;
+			this.time = 0;
+			this.theShortestPath = findThePath(this.nodeMap[(int)start.x / 3, (int)start.y / 2, (int)start.z / 3], this.nodeMap[(int)end.x / 3, (int)end.y / 2, (int)end.z / 3]);
+			this.theShortestPath.Reverse();
+			GC.Collect();
+		}
+
+		//int map to nodeMap for A*.
+		public Node[,,] mapToNodeMap(int[,,] map) {
+			Node[,,] nodeMap;
+			nodeMap = new Node[map.GetLength(0), map.GetLength(1), map.GetLength(2)];
+			for (var X = 0; X < map.GetLength(0); ++X) {
+				for (var Y = 0; Y < map.GetLength(1); ++Y) {
+					for (var Z = 0; Z < map.GetLength(2); ++Z)
+						nodeMap[X, Y, Z] = new Node(X, Y, Z, (nodeType)map[X, Y, Z]);
+				}
+			}
+			return nodeMap;
+		}
+
+		//find the shortest path.
+		public List<Node> findThePath(Node start, Node end) {
+			bool finish = false;
+			Stack<Node> answer = new Stack<Node>();
+			answer.Push(start);
+			while (!finish) {
+				var aroundPositions = findAround(answer.Peek());
+				var temp = answer.Peek();
+				for (var i = 0; i < aroundPositions.Count; ++i) {
+					aroundPositions[i].distanceToEnd(end, time);
+					//if fscore is same then select node by using hscore.
+					if (aroundPositions[i].fScore == temp.fScore && aroundPositions[i].hScore < temp.hScore) {
+						temp = aroundPositions[i];
+						continue;
+					}
+					//find the minimum fscore in the round.
+					if (aroundPositions[i].fScore < temp.fScore)
+						temp = aroundPositions[i];
+				}
+				answer.Push(temp);
+				time++;
+				if (time > 1000)
+					break;
+				if (temp.type == nodeType.end)
+					finish = true;
+			}
+			return answer.ToList();
+		}
+
+		//find the around node of input node.
+		public List<Node> findAround(Node node) {
+			List<Node> temp = new List<Node>();
+			for (var i = -1; i < 2; ++i) {
+				for (var j = -1; j < 2; ++j) {
+					for (var k = -1; k < 2; ++k) {
+						if (//skip out of range.
+							node.X + i < 0 || node.Y + j < 0 || node.Z + k < 0 ||
+							node.X + i >= nodeMap.GetLength(0) ||
+							node.Y + j >= nodeMap.GetLength(1) ||
+							node.Z + k >= nodeMap.GetLength(2) ||
+							//skip the origin node.
+							(i == 0 && j == 0 && k == 0))
+							continue;
+						if (nodeMap[node.X + i, node.Y + j, node.Z + k].type == nodeType.wall)
+							continue;
+						temp.Add(nodeMap[node.X + i, node.Y + j, node.Z + k]);
+					}
+				}
+			}
+			return temp;
+		}
+	}
+}
---
+++ Assets/WillDelete/Editor/GeneticAlgorithm/GeneticAlgorithm.cs
@@ -0,0 +1,292 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using Stopwatch = System.Diagnostics.Stopwatch;
+using GC = System.GC;
+using UnityEngine;
+using UnityEditor;
+using GeneticSharp.Domain;
+using GeneticSharp.Domain.Populations;
+using GeneticSharp.Domain.Chromosomes;
+using GeneticSharp.Domain.Selections;
+using GeneticSharp.Domain.Fitnesses;
+using GeneticSharp.Domain.Crossovers;
+using GeneticSharp.Domain.Mutations;
+using GeneticSharp.Domain.Terminations;
+
+using CreVox;
+
+namespace CrevoxExtend {
+	public class CreVoxGA {
+		private const int _generationNumber = 20;
+		private const float _enemyMutationRate = 5.0f;
+		private const float _treasureMutationRate = 2.0f;
+		private const float _trapMutationRate = 2.0f;
+		private const float _emptyMutationRate = 10.0f;
+		private const float _crossOverRate = 5.0f;
+		private const string _picecName = "Gnd.in.one";
+		private const int _volumeTraget = 0;
+		private static List<Volume> volumes;
+		private static GameObject genePos = new GameObject("genePos");
+		private static HashSet<Vector3> canWalk = new HashSet<Vector3>();
+
+		public enum GeneType {
+			Forbidden = -1,
+			Empty = 0,
+			Enemy = 1,
+			Treasure = 2,
+			Trap = 3
+		}
+
+		// Add the 'Level settings' in 'Dungeon' menu.
+		[MenuItem("Dungeon/Love GA", false, 99)]
+		public static void GoFighting() {
+			Segmentism();
+		}
+
+		// Add the 'test' in 'Dungeon' menu.
+		//test the A* for volume.
+		[MenuItem("Dungeon/test", false, 99)]
+		public static void test() {
+			var item = GameObject.Find("VolumeManger(Generated)").transform.FindChild("Entrance_01_vData");
+			var starpos = item.FindChild("ItemRoot").transform.FindChild("Starting Node").transform.position;
+			var endpos = item.FindChild("ItemRoot").transform.FindChild("Connection_Default").transform.position;
+			//Debug.Log(starpos);
+			//Debug.Log(endpos);
+			var map = makeAMap(item.gameObject);
+
+			//for (var y = 0; y < 9; ++y) {
+			//    string pp = "";
+			//    for (var x = 0; x < 9; ++x) {
+			//        for (var z = 0; z < 9; ++z) {
+			//            pp += map[x, y, z];
+			//        }
+			//        pp += "\n";
+			//    }
+			//    Debug.Log(pp);
+			//}
+
+			AStar tt = new AStar(map, starpos, endpos);
+			foreach (var i in tt.theShortestPath) {
+				Vector3 normalized = new Vector3(i.X / 3, i.Y / 2, i.Z / 3);
+				Debug.Log(i.ToString());
+			}
+		}
+
+		public static void Segmentism() {
+			Stopwatch sw = new Stopwatch();
+			volumes = getVolumeByVolumeManager();
+			foreach (var volume in volumes) {
+				int crossOverIndex1 = default(int);
+				int crossOverIndex2 = default(int);
+				var genePositions = GetPositionsByPicecName(_picecName, volume);
+				generateRandomCrossOverIndex(genePositions.Count, ref crossOverIndex1, ref crossOverIndex2);
+				//instance necessary class for GA.
+				var selection = new EliteSelection();
+				var crossover = new TwoPointCrossover(crossOverIndex1, crossOverIndex2);
+				var mutation = new MyMutation();
+				var fitness = new MyProblemFitness();
+				var chromosome = new MyProblemChromosome();
+				var population = new Population(50, 70, chromosome);
+				//execute GA.
+				var ga = new GeneticAlgorithm(population, fitness, selection, crossover, mutation);
+				ga.Termination = new GenerationNumberTermination(_generationNumber);
+				ga.CrossoverProbability = _crossOverRate;
+				Debug.Log("GA running...");
+				ga.Start();
+				Debug.Log("Best solution found has " + ga.BestChromosome.Fitness + " fitness.");
+				BestGeneToWorldPos(ga.BestChromosome);
+				//volumes[volumeTraget].name = "6666666";
+				//findTheShortestPath();
+				//Debug.Log(GameObject.Find("connection").transform.position);
+			}
+			sw.Stop();
+			Debug.Log(sw.ElapsedMilliseconds + " ms");
+			GC.Collect();
+		}
+
+		//generate crossOverIndex1 and crossOverIndex2.
+		public static void generateRandomCrossOverIndex(int volumeLength, ref int crossOverIndex1, ref int crossOverIndex2) {
+			var randomUpperBound = volumeLength - 1;
+			crossOverIndex1 = Random.Range(0, randomUpperBound);
+			if (crossOverIndex1 == 0)
+				crossOverIndex2 = Random.Range(1, randomUpperBound);
+			else if (crossOverIndex1 == randomUpperBound - 1) {
+				crossOverIndex2 = crossOverIndex1;
+				crossOverIndex1 = Random.Range(0, crossOverIndex2);
+			}
+			else
+				crossOverIndex2 = Random.Range(crossOverIndex1 + 1, randomUpperBound);
+		}
+
+		// use volumeManger to find all of volumes.
+		public static List<Volume> getVolumeByVolumeManager() {
+			List<Volume> volumes = new List<Volume>();
+			var volumeManger = GameObject.Find("VolumeManger(Generated)");
+			for (int i = 0; i < volumeManger.transform.childCount; ++i) {
+				volumes.Add(volumeManger.transform.GetChild(i).GetComponent<Volume>());
+			}
+			return volumes;
+		}
+
+		// use volume to get each block position.
+		public static List<Vector3> GetPositionsByPicecName(string blockAirPieceName, Volume volume) {
+			List<Vector3> positions = new List<Vector3>();
+			//use volume to find DecorationRoot and find the DecorationRoot's child.
+			var decorationRoots = volume.gameObject.transform.FindChild("DecorationRoot");
+			for (int i = 0; i < decorationRoots.childCount; ++i) {
+				var temp = decorationRoots.GetChild(i).FindChild(blockAirPieceName);
+				if (temp == null)
+					continue;
+				positions.Add(temp.position);
+				canWalk.Add(temp.position);
+			}
+			return positions;
+		}
+
+		//make the best gene is added into world.
+		public static void BestGeneToWorldPos(IChromosome bestGene) {
+			foreach (var gene in bestGene.GetGenes()) {
+				genePos = GameObject.Find("genePos") ?? new GameObject("genePos");
+				GameObject geneWorldPosition = GameObject.CreatePrimitive(PrimitiveType.Cube);
+				geneWorldPosition.transform.SetParent(genePos.transform);
+				geneWorldPosition.transform.position = (gene.Value as CreVoxGene).Position;
+			}
+		}
+
+		//make current volume to a 3d int array map for A*.
+		public static int[,,] makeAMap(GameObject volume) {
+			int[,,] temps = new int[9, 9, 9];
+			var decorationRoots = volume.transform.FindChild("DecorationRoot");
+			for (int i = 0; i < decorationRoots.childCount; ++i) {
+				var temp = decorationRoots.GetChild(i).FindChild(_picecName).position;
+				if (temp == null)
+					continue;
+				temps[(int)temp.x / 3, (int)temp.y / 2, (int)temp.z / 3] = 1;
+			}
+			return temps;
+		}
+
+		//implement necessary class for GA.
+		//implement MyProblemFitness for IFitness.
+		public class MyProblemFitness : IFitness {
+			public double Evaluate(IChromosome chromosome) {
+				double fitnessValue = default(double);
+
+				fitnessValue += FitnessTrap(chromosome)
+							 + FitnessTreasure(chromosome)
+							 + FitnessDominator(chromosome);
+
+				return fitnessValue;
+			}
+
+			public double FitnessTrap(IChromosome chromosome) {
+				double fitnessScore = 0;
+				var enemies = chromosome.GetGenes().Where(g => (g.Value as CreVoxGene).Type == GeneType.Enemy).ToList();
+				var traps = chromosome.GetGenes().Where(g => (g.Value as CreVoxGene).Type == GeneType.Trap).ToList();
+
+				foreach (var enemy in enemies) {
+					var enemyGene = enemy.Value as CreVoxGene;
+					foreach (var trap in traps) {
+						var trapGene = trap.Value as CreVoxGene;
+						var distance = (enemyGene.Position - trapGene.Position).magnitude;
+						fitnessScore += (distance == 0) ? 0 : 1 / distance;
+					}
+				}
+				return fitnessScore;
+			}
+
+			public double FitnessTreasure(IChromosome chromosome) {
+				double fitnessScore = 0;
+				var enemies = chromosome.GetGenes().Where(g => (g.Value as CreVoxGene).Type == GeneType.Enemy).ToList();
+				var treasures = chromosome.GetGenes().Where(g => (g.Value as CreVoxGene).Type == GeneType.Treasure).ToList();
+
+				foreach (var enemy in enemies) {
+					var enemyGene = enemy.Value as CreVoxGene;
+					foreach (var treasure in treasures) {
+						var treasureGene = treasure.Value as CreVoxGene;
+						var distance = (enemyGene.Position - treasureGene.Position).magnitude; fitnessScore += (distance == 0) ? 0 : 1 / distance;
+						fitnessScore += (distance == 0) ? 0 : 1 / distance;
+					}
+				}
+				return fitnessScore;
+			}
+
+			public double FitnessDominator(IChromosome chromosome) {
+				double fitnessScore = 0;
+				var enemies = chromosome.GetGenes().Where(g => (g.Value as CreVoxGene).Type == GeneType.Enemy).ToList();
+
+				foreach (var enemy in enemies) {
+					var enemyGene = enemy.Value as CreVoxGene;
+					fitnessScore += enemyGene.Position.y / 8.0f;
+				}
+				return fitnessScore;
+			}
+		}
+
+		//implement MyProblemChromosome for ChromosomeBase.
+		public class MyProblemChromosome : ChromosomeBase {
+			public MyProblemChromosome() : base(GetPositionsByPicecName(_picecName, volumes[_volumeTraget]).Count) {
+				CreateGenes();
+			}
+
+			public override IChromosome CreateNew() {
+				return new MyProblemChromosome();
+			}
+
+			protected override void CreateGenes() {
+				var genes = GetPositionsByPicecName(_picecName, volumes[_volumeTraget]);
+				int index = 0;
+				foreach (var gene in genes) {
+					this.ReplaceGene(index, new Gene(new CreVoxGene(GeneType.Empty, gene)));
+					index++;
+				}
+			}
+
+			public override Gene GenerateGene(int geneIndex) {
+				GeneType type = GeneType.Forbidden;
+				return new Gene(type);
+			}
+		}
+
+		//implement MyMutation for IMutation.
+		public class MyMutation : IMutation {
+			public bool IsOrdered {
+				get;
+				private set;
+			}
+
+			public void Mutate(IChromosome chromosome, float probability) {
+				var genes = chromosome.GetGenes();
+				foreach (var gene in genes) {
+					var CVGene = gene.Value as CreVoxGene;
+					var seed = Random.Range(0.0f, 100.0f);
+					if (seed < _enemyMutationRate) {
+						CVGene.Type = GeneType.Enemy;
+						continue;
+					}
+					seed -= _enemyMutationRate;
+					if (seed < _treasureMutationRate) {
+						CVGene.Type = GeneType.Treasure;
+						continue;
+					}
+					seed -= _treasureMutationRate;
+					if (seed < _trapMutationRate) {
+						CVGene.Type = GeneType.Trap;
+						continue;
+					}
+				}
+			}
+		}
+
+		//using CreVoxGene to be gene of GA.
+		public class CreVoxGene {
+			public GeneType Type { get; set; }
+			public Vector3 Position { get; private set; }
+			// Constructor.
+			public CreVoxGene(GeneType type, Vector3 position) {
+				this.Type = type;
+				this.Position = position;
+			}
+		}
+	}
+}
