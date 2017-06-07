4372684399b1f2e9f8334683b1e71e4cca28073a
Salmon <salmon.zh.tw@gmail.com>
2017-06-07 23:02:01 +0800

    Layout of csv is okay.

--- Assets/Resources/GeneticAlgorithmExperiment/.PythonPlot/web/main.js
+++ Assets/Resources/GeneticAlgorithmExperiment/.PythonPlot/web/main.js
@@ -4,43 +4,53 @@ const fs      = require('fs');
 const _       = require('underscore');
 const open    = require('open');
 const express = require('express');
+const parser  = require('csv-parse');
 const app     = express();
 
 let LISTEN_PORT = 80;
+let rawDatasetPath = "C:/Users/Salmon/AppData/LocalLow/DefaultCompany/Voxel2/Experiments/Exports/BestChromosome/";
 
 // // Open the browser.
 // open('http://localhost:' + LISTEN_PORT + '/fitnessfunctions');
 
-fs.readdirSync("C:/Users/Salmon/AppData/LocalLow/DefaultCompany/Voxel2/Experiments/Exports/BestChromosome").forEach(file => {
+// Set the HTML template engine.
+app.set('view engine', 'pug');
+// Set the static folders.
+app.use(express.static('public'));
+
+fs.readdirSync(rawDatasetPath).forEach(file => {
 	let matched = file.match(/^bestChromosome_(\w+)_(\d+)_(\d+).csv$/);
 	if (matched) {
 		let metrics     = matched[1];
 		let runs        = matched[2];
-		let chromosomes = matched[3];
-		console.log(metrics, runs, chromosomes);
-	}
-})
+		let individuals = matched[3];
 
-app.get('/fitnessfunctions/', function(req, res) {
-	res.send("hello world.");
+		console.log(metrics, runs, individuals);
+	}
 });
 
-app.get('/fitnessfunctions/:metrics', function(req, res) {
+app.get('/fitnessfunctions/', (req, res) => {
+	res.send(rawDatasetPath);
+});
+
+app.get('/fitnessfunctions/:metrics', (req, res) => {
 	const params = req.params;
 	// Fields.
 	let metrics = req.params['metrics'];
+	let csvPath = rawDatasetPath;
 
 	// Select the experiments.
-	switch (metrics) {
-	case 'A':
-		break;
-	default:
-		break;
-	}
-	res.send("done");
+	csvPath += './bestChromosome_' + metrics + '.csv';
+	fs.readFile(csvPath, 'utf8', (err, input) => {
+		if (err) {
+			res.render('index', { 'fitnessscores': [] });
+		} else {
+			parser(input, (err, output) => res.render('index', { 'fitnessscores': output }));
+		}
+	});
 });
 
-app.listen(LISTEN_PORT, function () {
+app.listen(LISTEN_PORT, () => {
 	// Start listening.
 	console.log('Listening on port ' + LISTEN_PORT + '.');
 });
