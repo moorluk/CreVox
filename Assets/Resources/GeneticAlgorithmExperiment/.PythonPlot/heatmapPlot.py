e47c8f939b232b7a7e38badda322cc67c3fe99b3
Salmon <salmon.zh.tw@gmail.com>
2017-06-23 00:25:01 +0800

    Set NaN values to ignore the uncontributed values.

--- Assets/Resources/GeneticAlgorithmExperiment/.PythonPlot/heatmapPlot.py
+++ Assets/Resources/GeneticAlgorithmExperiment/.PythonPlot/heatmapPlot.py
@@ -31,21 +31,31 @@ def main():
 			chromosomes = amountRecord[(amountRecord['run'] == runNumber) & (amountRecord['generation'] == generationNumber)]
 			chromosomes = chromosomes[['run', 'generation', 'emptyCount', 'enemyCount', 'totalCount']]
 
-			positionCount = len(chromosomes) + 1
-			geneBound     = chromosomes.totalCount.max()
-			enemyBound    = chromosomes.enemyCount.max()
+			positionCount = len(chromosomes)
+			geneBound     = chromosomes['totalCount'].max()
+			enemyBound    = amountRecord['enemyCount'].max()
 
 			# Create the empty 2D list with the columns and rows.
-			heatmapData = [([0] * (geneBound + 1)) for _ in range(geneBound + 1)]
-
+			heatmapData = np.array([([0] * (geneBound + 1)) for _ in range(enemyBound + 1)], dtype=np.float)
+			# Update the appearence times.
 			for idx, chromosome in chromosomes.iterrows():
-				heatmapData[chromosome.enemyCount][chromosome.emptyCount] += 1.0 / positionCount
+				heatmapData[chromosome.enemyCount][chromosome.emptyCount] += 1.0
+			# Make the zero to the NaN.
+			heatmapData[heatmapData == 0] = np.nan
 
-			# Export the heatmap figure.
-			figure = plt.figure()
-			plt.imshow(heatmapData, cmap='coolwarm', interpolation='nearest')
-			plt.savefig("{}/{}_{}_result.png".format(os.getcwd(), runNumber, generationNumber))
-			plt.close(figure)
+			# Create the heatmap figure.
+			fig, ax = plt.subplots()
+			cax = ax.imshow(heatmapData, cmap='coolwarm', interpolation='nearest', vmin=0, vmax=positionCount)
+			# Set the title and labels.
+			ax.set_title('run ({}), generation ({})'.format(runNumber, generationNumber))
+			ax.set_xlabel("Empty tile amount")
+			ax.set_ylabel("Enemy tile amount")
+			# Horizontal colorbar.
+			cbar = fig.colorbar(cax, ticks=[0, positionCount / 2, positionCount]) #, orientation='horizontal')
+			cbar.ax.set_xticklabels(['Low', 'Medium', 'High'])
+			# Save the figure and close it.
+			plt.savefig("{}/{}_{}_result.png".format(os.getcwd(), runNumber, generationNumber), dpi=300)
+			plt.close(fig)
 			print ('Rendered the heatmap figure. Run ({}), generation ({}).'.format(runNumber, generationNumber));
 
 
@@ -53,7 +63,7 @@ def getAmountRecord(data):
 	amountRecord = pd.DataFrame()
 
 	for runNumber in range(1, data['run'].max() + 1):
-		for generationNumber in range(1, 10): #range(1, data['generation'].max() + 1):
+		for generationNumber in range(1, data['generation'].max() + 1):
 			print "Current state: run is {}, generation is {}.".format(runNumber, generationNumber)
 			for chromosomeNumber in range(1, data['chromosome'].max() + 1):
 				# Get the chromosome info
