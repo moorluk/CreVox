62a4cb3e3d150952534652a7afe882d6ed7cb174
Salmon <salmon.zh.tw@gmail.com>
2017-06-23 09:25:30 +0800

    Add getRangedList.

--- Assets/Resources/GeneticAlgorithmExperiment/.PythonPlot/heatmapPlot.py
+++ Assets/Resources/GeneticAlgorithmExperiment/.PythonPlot/heatmapPlot.py
@@ -10,6 +10,7 @@ from pandas import Series, DataFrame
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 # Our program.
 def main():
@@ -46,15 +47,16 @@ def main():
 
 			# Create the heatmap figure.
 			fig, ax = plt.subplots(figsize=(7.0, 3.0))
-			plt.subplots_adjust(right=1.0)
-			cax = ax.imshow(heatmapData, cmap='rainbow', interpolation='nearest', vmin=0, vmax=positionCount)
+			im = ax.imshow(heatmapData, cmap='rainbow', interpolation='nearest', vmin=0, vmax=positionCount)
 			# Set the title and labels.
 			ax.set_title('run ({}), generation ({})'.format(runNumber, generationNumber))
 			ax.set_xlabel("Empty tile amount")
 			ax.set_ylabel("Enemy tile amount")
-			# Horizontal colorbar.
-			cbar = fig.colorbar(cax, ticks=[0, positionCount / 2, positionCount]) #, orientation='horizontal')
-			cbar.ax.set_xticklabels(['Low', 'Medium', 'High'])
+			# Colorbar.
+			divider = make_axes_locatable(ax)
+			cax = divider.append_axes("right", size="5%", pad=0.05)
+			cbar = fig.colorbar(im, cax=cax, ticks=getRangedList(0, positionCount, 5))
+			## cbar.ax.set_yticklabels(['Low', 'Medium', 'High'])
 			# Save the figure and close it.
 			plt.savefig("{}/{}_{}_result.png".format(os.getcwd(), runNumber, generationNumber), dpi=300)
 			plt.close(fig)
@@ -63,8 +65,8 @@ def main():
 def getAmountRecord(data):
 	amountRecord = pd.DataFrame()
 
-	for runNumber in range(1, data['run'].max() + 1):
-		for generationNumber in range(1, data['generation'].max() + 1):
+	for runNumber in range(1, 2): #data['run'].max() + 1):
+		for generationNumber in range(1, 4): #data['generation'].max() + 1):
 			print "Current state: run is {}, generation is {}.".format(runNumber, generationNumber)
 			for chromosomeNumber in range(1, data['chromosome'].max() + 1):
 				# Get the chromosome info
@@ -87,11 +89,16 @@ def getAmountRecord(data):
 
 	# Release the memory.
 	gc.collect()
-
 	return amountRecord
 
 def getChromosome(data, runNum, genNum, chmNum):
 	return data[(data.run == runNum) & (data.generation == genNum) & (data.chromosome == chmNum)]
+
+def getRangedList(minVal, maxVal, sectionCount):
+	rangedList = [minVal]
+	for idx in range(1, sectionCount):
+		rangedList.append(float(maxVal - minVal) / (sectionCount - 1) * idx)
+	return [int(i) for i in rangedList]
 
 if __name__ == "__main__":
     main()
