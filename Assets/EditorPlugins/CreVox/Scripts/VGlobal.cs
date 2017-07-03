fc4abf9f89a6aeff19280862ce245ed05822227c
armyxxvii <armyxxvii@gmail.com>
2017-07-03 12:26:34 +0800

    fix SpaceAlphabet Method

--- Assets/EditorPlugins/CreVox/Scripts/VGlobal.cs
+++ Assets/EditorPlugins/CreVox/Scripts/VGlobal.cs
@@ -168,8 +168,7 @@ namespace CreVox
 				seed = UnityEngine.Random.Range (0, int.MaxValue);
 			if (_s.XmlPath.Length > 0 && _s.VGXmlPath.Length > 0) {
 				CreVoxNode root = CreVoxAttach.GenerateMissionGraph (PathCollect.gram + "/" + _s.XmlPath, seed);
-				Debug.Log ("_s.VGXmlPath of GenerateRealLevel" + _s.VGXmlPath);
-//				SpaceAlphabet.RuntimeGenerate(_s.SpaceXmlPath);
+				SpaceAlphabet.RuntimeGenerate(_s.SpaceXmlPath);
 				return CrevoxGeneration.GenerateRealLevel(root, _s, seed);
 			} else {
 				return false;
