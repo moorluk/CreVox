b1e598876a7b310559b47554abdebc00a66119e6
27.army <armyxxvii@gmail.com>
2017-07-10 09:46:55 +0800

    資源更新

--- Assets/EditorPlugins/CreVox/Extension/VolumeAdapter.cs
+++ Assets/EditorPlugins/CreVox/Extension/VolumeAdapter.cs
@@ -5,8 +5,17 @@ public class VolumeAdapter {
 
 	public static void AfterVolumeInit(GameObject volume)
     {
+		//event system
 		Type eventDriver = Type.GetType ("RoomDriver");
 		if (eventDriver != null && volume.GetComponent(eventDriver) == null)
 			volume.AddComponent(eventDriver);
+		//SECTR
+		Type sectr = Type.GetType ("SECTR_Sector");
+		if (sectr != null && volume.GetComponent (sectr) == null) {
+			volume.AddComponent (sectr);
+//			SECTR_Sector ss = volume.GetComponent (sectr) as SECTR_Sector;
+//			ss.ChildCulling = SECTR_Member.ChildCullModes.Individual;
+		}
+
     }
 }
