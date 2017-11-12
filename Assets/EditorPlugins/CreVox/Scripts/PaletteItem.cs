ed4de6f500ece3839cfed1e7c8dc5edc960ce035
armyxxvii <armyxxvii@gmail.com>
2017-11-13 07:49:27 +0800

    調整 Markertype

--- Assets/EditorPlugins/CreVox/Scripts/PaletteItem.cs
+++ Assets/EditorPlugins/CreVox/Scripts/PaletteItem.cs
@@ -3,37 +3,39 @@
 namespace CreVox
 {
 
-	public class PaletteItem : MonoBehaviour
-	{
-		public enum Category
-		{
-			Build,
-			Deco,
-			System,
-			Trap,
-			Sign,
-			Movement,
-			Chara,
-			Obstacle
-		}
-		public enum MarkerType
-		{
-			Ground,
-			Wall,
-			WallSeparator,
-			Fence,
-			FenceSeparator,
-			Door,
-			Stair,
-//			Stairhalf,
-//			WallHalf,
-//			WallHalfSeparator,
-			Item
-		}
+    public class PaletteItem : MonoBehaviour
+    {
+        public enum Category
+        {
+            Build,
+            Deco,
+            System,
+            Trap,
+            Sign,
+            Movement,
+            Chara,
+            Obstacle
+        }
 
-		public Category category = Category.System;
-		public MarkerType markType = MarkerType.Item;
-		public string itemName = "";
+        public enum MarkerType
+        {
+            Item = -3,
+            Ground = -2,
+            Stair = -1,
+            Door = 0,
+            Wall = 1,
+            WallSeparator,
+            Fence,
+            FenceSeparator,
+            Roof,
+            //Stairhalf,
+            //WallHalf,
+            //WallHalfSeparator,
+        }
+
+        public Category category = Category.System;
+        public MarkerType markType = MarkerType.Item;
+        public string itemName = "";
         public LevelPiece inspectedScript;
         public string assetPath;
     }
